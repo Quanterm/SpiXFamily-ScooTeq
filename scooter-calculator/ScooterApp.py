fbc87ffd9608c4c1be6bed1b161d3062cf65245b
vieta <vieta@root533.premium-rootserver.net>
2023-01-26 22:37:11 +0100

    TODO add API

--- scooter-calculator/ScooterApp.py
+++ scooter-calculator/ScooterApp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import ScooterCost as sc
 
 ScooterApp = Flask(__name__)
+#Run the app with "flask --app ScooterApp.py run" don`t forget you need to install the flask module with "pip install flask"
 
 
 @ScooterApp.route("/", methods=["GET", "POST"])
@@ -18,4 +19,12 @@ def kilometerRoute():
         kilometer = int(request.form["kilometer"])
         cost = sc.ScooterKilometer(kilometer)
         return render_template("result2.html", kilometer=kilometer, cost=cost)
+    return render_template("index.html")
+
+@ScooterApp.route("/minutes", methods=["GET", "POST"])
+def minuteRoute():
+    if request.method == "POST":
+        minutes = int(request.form["minutes"])
+        cost = sc.ScooterMinute(minutes)
+        return render_template("result.html", minutes=minutes, cost=cost)
     return render_template("index.html")
